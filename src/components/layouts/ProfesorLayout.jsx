8ca25f319bf6841a9be3767821d317a80d2ea18e
Jose Marquez <josemmarquezlven@gmail.com>
2023-03-04 14:50:27 -0400

    remove test token

--- src/components/layouts/ProfesorLayout.jsx
+++ src/components/layouts/ProfesorLayout.jsx
@@ -1,17 +1,12 @@
-import React from "react";
-import axios from 'axios'
+import React, { useEffect } from "react";
+import axios from "axios";
 
 import { css } from "@emotion/react";
 import { Outlet } from "react-router-dom";
 
 import Navbar from "../Navbar";
 
-const token =
-  "MQ.ucAVxFMQoK6-MWQmYvDLTb2cziLUD_p4E9HtqHbCeNRSYt8kKhOHL-eqkJNJ";
-axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-
 function ProfesorLayout() {
-
   return (
     <div>
       <Navbar names={["Datos", "Casos"]} />
