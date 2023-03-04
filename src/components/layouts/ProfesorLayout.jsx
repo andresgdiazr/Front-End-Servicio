b62e05cfd056a8d38778699e8ad007399e0e88c2
Jose Marquez <josemmarquezlven@gmail.com>
2023-03-04 14:52:32 -0400

    add dev token as a comment

--- src/components/layouts/ProfesorLayout.jsx
+++ src/components/layouts/ProfesorLayout.jsx
@@ -1,12 +1,17 @@
-import React, { useEffect } from "react";
-import axios from "axios";
+import React from "react";
+import axios from 'axios'
 
 import { css } from "@emotion/react";
 import { Outlet } from "react-router-dom";
 
 import Navbar from "../Navbar";
 
+// const token =
+//   "MTA.B9HLo0qaLADyck5mHm_1YbElDDrzsBM-BWjG4HpZXKWt2IoQEhaKedIaCQJ3";
+// axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
 function ProfesorLayout() {
+
   return (
     <div>
       <Navbar names={["Datos", "Casos"]} />
