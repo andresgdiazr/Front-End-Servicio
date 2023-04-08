1745fb8216728ff1a08fab973dfeff11e6e05fe6
ReynerCr <25756546+ReynerCr@users.noreply.github.com>
2023-04-08 19:39:04 -0400

    DRY Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import {
-  Box,
-  AppBar,
-  Toolbar,
-  Typography,
-  IconButton,
-  styled,
-  css,
+	Box,
+	AppBar,
+	Toolbar,
+	Typography,
+	IconButton,
+	styled,
 } from "@mui/material";
 
 import MenuIcon from "@mui/icons-material/Menu";
@@ -14,67 +13,64 @@ import MenuIcon from "@mui/icons-material/Menu";
 import { useNavigate } from "react-router-dom";
 
 const MenuButton = styled(Typography)(({ theme }) => ({
-  marginRight: theme.spacing(2),
+	marginRight: theme.spacing(2),
+  fontSize: "1.2rem",
+  fontWeight: "bold",
+  cursor: "pointer",
+  alignSelf: "center",
 }));
 
 const Container = styled(Typography)(({ theme }) => ({
-  offset: theme.mixins.toolbar,
+	offset: theme.mixins.toolbar,
 }));
 
-// TODO debe haber una manera de que esto sea dinámico o si no se debe eliminar
-const defaultNames = [
-  ["Inicio", "/"],
-];
+// TODO debe haber una manera de que esto sea dinámico, o si no se debe eliminar
+const defaultNames = [["Inicio", "/"]];
 
 const Navbar = ({ names = defaultNames }) => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const navegador = (data) => {
-    navigate(data, { state: names });
-  };
+	const navegador = (data) => {
+		navigate(data, { state: names });
+	};
 
-  const onLogout = () => {
-    sessionStorage.clear();
-    navigate("/login", { replace: true });
-  };
+	const onLogout = () => {
+		sessionStorage.clear();
+		navigate("/login", { replace: true });
+	};
 
-  return (
-    <div>
-      <AppBar position="fixed" color="primary">
-        <Toolbar>
-          <Box sx={{ flexGrow: 3, display: "flex", alignItems: "center" }}>
-            <IconButton color="inherit" aria-label="menu" sx={{ spacing: 2 }}>
-              <MenuIcon />
-            </IconButton>
-            {names.map((name, idx) => {
-              return (
-                <MenuButton
-                  key={idx}
-                  onClick={() => {
-                    navegador(name[1]);
-                  }}
-                  variant="h6"
-                >
-                  {name[0]}
-                </MenuButton>
-              );
-            })}
-          </Box>
-          <MenuButton variant="h6">Mi cuenta</MenuButton>
-          <Typography
-            css={css`
-              cursor: pointer;
-            `}
-            onClick={onLogout}
-            variant="h6"
-          >
-            Cerrar Sesión
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Container></Container>
-    </div>
-  );
+	return (
+		<div>
+			<AppBar position="fixed" color="primary">
+				<Toolbar>
+					<Box sx={{ flexGrow: 1, display: "flex" }}>
+						<IconButton color="inherit" aria-label="menu">
+							<MenuIcon />
+						</IconButton>
+						{names.map((name, idx) => {
+							return (
+								<MenuButton
+									key={idx}
+                  component="a"
+									onClick={() => {
+										navegador(name[1]);
+									}}
+								>
+									{name[0]}
+								</MenuButton>
+							);
+						})}
+					</Box>
+					<Box sx={{ flexGrow: 0, display: "flex" }}>
+						<MenuButton component="a" onClick={onLogout}>
+							Cerrar Sesión
+						</MenuButton>
+					</Box>
+				</Toolbar>
+			</AppBar>
+			<Container></Container>
+		</div>
+	);
 };
 
 export default Navbar;
