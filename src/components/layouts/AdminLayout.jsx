57dbc190f423d9c72e649c822f7848e05ce33dfd
ReynerCr <25756546+ReynerCr@users.noreply.github.com>
2023-04-07 22:24:40 -0400

    Refactor: crear cuentas ahora es por tipo por ruta

--- src/components/layouts/AdminLayout.jsx
+++ src/components/layouts/AdminLayout.jsx
@@ -1,29 +1,29 @@
 import React from "react";
-import {css} from "@emotion/react";
-import {Outlet} from "react-router-dom";
+import { css } from "@emotion/react";
+import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar";
 
-function AdminLayout(){
-
-    const navbar = [
+function AdminLayout() {
+	const navbar = [
 		["Profesor", "/dashboard-control/admin/profesores"],
 		["Seccion", "admin/secciones"],
 		["Materia", "admin/profesores"],
 	];
 
-    return(
-        <div>
-            <Navbar names={navbar} />
-            <div
-                css={css`
-                    margin-top:64px;
-                    padding: 1rem 2rem;
-                `}
-            >
-                <Outlet/>
-            </div>
-        </div>
-    )
+	return (
+		<div>
+			<Navbar names={navbar} />
+			<div
+				css={css`
+					margin-top: 64px;
+					padding: 1rem 2rem;
+				`}
+			>
+				<GoBackButton to="prev" />
+				<Outlet />
+			</div>
+		</div>
+	);
 }
 
 export default AdminLayout;
---
+++ src/routes/admin/CuentaModificar.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate, useLocation } from "react-router-dom";
+import { updateCuenta } from "../../api/updateCuenta";
+import GoBackButton from "../../components/atoms/GoBackButton";
+import CuentaForm from "../../components/organisms/CuentaForm";
+import { setLoading } from "../../store/features/main";
+import { Alert, css, Snackbar } from "@mui/material";
+
+function CuentaModificar({ tipo }) {
+	const navigate = useNavigate();
+
+	const [usedEmails, setUsedEmails] = useState([]);
+	const [usedCedulas, setUsedCedulas] = useState([]);
+
+	const { state } = useLocation();
+	const [open, setOpen] = useState(false);
+
+	const dispatch = useDispatch();
+
+	const onSubmit = async (data) => {
+		dispatch(setLoading(true));
+		const response = await updateCuenta(tipo, state.id, data);
+		if (response.status == 200) {
+			setOpen(true);
+			dispatch(setLoading(false));
+			//navigate(-1); // Ir hacia atras, si se quita el comentario no se mostrara el snackbar
+		} else {
+			if (
+				response.data.errors.some(
+					(error) => error.field === "email" && error.rule === "unique"
+				)
+			) {
+				setUsedEmails([...usedEmails, data.email]);
+			}
+			if (
+				response.data.errors.some(
+					(error) => error.field === "cedula" && error.rule === "unique"
+				)
+			) {
+				setUsedCedulas([...usedCedulas, data.cedula]);
+			}
+		}
+		dispatch(setLoading(false));
+	};
+
+	return (
+		<div>
+			<Snackbar
+				css={css`
+					svg {
+						color: white;
+					}
+				`}
+				open={open}
+				autoHideDuration={1000}
+				onClose={() => setOpen(false)}
+			>
+				<Alert variant="filled" severity="success">
+					Cuenta modificada satisfactoriamente
+				</Alert>
+			</Snackbar>
+			<GoBackButton to="prev" />
+			<h2>Administración de {tipo}</h2>
+			<h3>Modificando información de la cuenta</h3>
+
+			<CuentaForm
+				onSubmit={onSubmit}
+				usedEmails={usedEmails}
+				usedCedulas={usedCedulas}
+			/>
+		</div>
+	);
+}
+
+export default CuentaModificar;
