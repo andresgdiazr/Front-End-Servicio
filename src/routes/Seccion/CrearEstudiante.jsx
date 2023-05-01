7caa19ef7e13b4e86d13f3177ad66781707f9790
Jose Marquez <josemmarquezlven@gmail.com>
2023-05-01 19:22:32 -0400

    refactor CrearEstudiante.jsx to fix ux bug with component re-render

--- src/routes/Seccion/CrearEstudiante.jsx
+++ src/routes/Seccion/CrearEstudiante.jsx
@@ -25,129 +25,54 @@ function CrearEstudiante() {
     register,
     handleSubmit,
     setError,
-		clearErrors,
+    clearErrors,
     control,
     formState: { errors },
   } = useForm({
-		defaultValues:{
-			sexo: "M"
-		}
-	});
+    defaultValues: {
+      sexo: "M",
+    },
+  });
 
-  const FormatoCrearEstudiante = () => {
-    const [secciones, setSecciones] = useState("");
+  const [secciones, setSecciones] = useState("");
 
-    useEffect(() => {
-      const fetchSecciones = async () => {
-        const SeccionesRes = await getSecciones();
-        setSecciones(SeccionesRes);
-      };
-
-      fetchSecciones();
-    }, []);
-
-    const onSubmit = (data) => {
-
-      const año = secciones.filter((s) => s.id == seccionId)[0].año;
-
-      axios
-        .post(`/admin/estudiantes`, {
-          nombre: data.nombre,
-          apellido: data.apellido,
-          sexo: data.sexo,
-          año,
-          egresado: false,
-          cedula: data.cedula,
-          seccion_id: parseInt( seccionId ),
-        })
-        .then((res) => {
-          console.log(res);
-          alert("Estudiante creado con exito");
-        })
-        .catch((error) => {
-          if(error.response.data?.errors.some( e => e.rule === 'unique' && e.field == 'cedula' ) ) {
-						setError("cedula-duplicate-error", {
-							type: "custom",
-							message: "Ya existe un estudiante con esta cedula",
-						});
-					}
-        });
-
+  useEffect(() => {
+    const fetchSecciones = async () => {
+      const SeccionesRes = await getSecciones();
+      setSecciones(SeccionesRes);
     };
 
-    return (
-      <>
-        <CustomForm onSubmit={handleSubmit(onSubmit)}>
-          <TextInput
-            label="Nombres del estudiante"
-            placeholder="Pedro"
-            id="student-name"
-            reactHookProps={register("nombre", {
-              validate: (value) =>
-                !value.trim() ? "este campo es requerido" : true,
-            })}
-            error={errors?.nombre?.message}
-          />
+    fetchSecciones();
+  }, []);
+  const onSubmit = (data) => {
+    const año = secciones.filter((s) => s.id == seccionId)[0].año;
 
-          <TextInput
-            label="Apellidos del estudiante"
-            placeholder="Perez"
-            id="student-apellido"
-            reactHookProps={register("apellido", {
-              validate: (value) =>
-                !value.trim() ? "este campo es requerido" : true,
-            })}
-            error={errors?.apellido?.message}
-          />
-
-          <TextInput
-            label="Cedula del estudiante"
-            placeholder="1234567"
-            id="student-cedula"
-            reactHookProps={register("cedula", {
-              validate: (value) => {
-                if (!value.trim()) {
-                  return "este campo es requerido";
-                } else if (!/^\d+$/.test(value)) {
-                  return "cedula invalida";
-                }
-
-                return true;
-              },
-							onChange:() => clearErrors("cedula-duplicate-error")
-            })}
-            error={errors?.cedula?.message || errors?.["cedula-duplicate-error"]?.message}
-          />
-
-          <Controller
-            control={control}
-            name="sexo"
-            render={({ field: { onChange, value, ref } }) => (
-              <SelectInput
-                label="Género"
-                id="student-sexo"
-                value={value}
-                inputRef={ref}
-                onChange={onChange}
-                options={[
-                  { value: "M", display: "Masculino" },
-                  { value: "F", display: "Femenino" },
-                ]}
-              />
-            )}
-          />
-
-          <Button
-            size="large"
-            variant="contained"
-            color="success"
-            type="submit"
-          >
-            Añadir estudiante
-          </Button>
-        </CustomForm>
-      </>
-    );
+    axios
+      .post(`/admin/estudiantes`, {
+        nombre: data.nombre,
+        apellido: data.apellido,
+        sexo: data.sexo,
+        año,
+        egresado: false,
+        cedula: data.cedula,
+        seccion_id: parseInt(seccionId),
+      })
+      .then((res) => {
+        console.log(res);
+        alert("Estudiante creado con exito");
+      })
+      .catch((error) => {
+        if (
+          error.response.data?.errors.some(
+            (e) => e.rule === "unique" && e.field == "cedula"
+          )
+        ) {
+          setError("cedula-duplicate-error", {
+            type: "custom",
+            message: "Ya existe un estudiante con esta cedula",
+          });
+        }
+      });
   };
 
   return (
@@ -155,7 +80,73 @@ function CrearEstudiante() {
       <Typography>Administración de secciones</Typography>
       <Typography>Ingrese la información del nuevo estudiante</Typography>
 
-      <FormatoCrearEstudiante />
+      <CustomForm onSubmit={handleSubmit(onSubmit)}>
+        <TextInput
+          label="Nombres del estudiante"
+          placeholder="Pedro"
+          id="student-name"
+          reactHookProps={register("nombre", {
+            validate: (value) =>
+              !value.trim() ? "este campo es requerido" : true,
+          })}
+          error={errors?.nombre?.message}
+        />
+
+        <TextInput
+          label="Apellidos del estudiante"
+          placeholder="Perez"
+          id="student-apellido"
+          reactHookProps={register("apellido", {
+            validate: (value) =>
+              !value.trim() ? "este campo es requerido" : true,
+          })}
+          error={errors?.apellido?.message}
+        />
+
+        <TextInput
+          label="Cedula del estudiante"
+          placeholder="1234567"
+          id="student-cedula"
+          reactHookProps={register("cedula", {
+            validate: (value) => {
+              if (!value.trim()) {
+                return "este campo es requerido";
+              } else if (!/^\d+$/.test(value)) {
+                return "cedula invalida";
+              }
+
+              return true;
+            },
+            onChange: () => clearErrors("cedula-duplicate-error"),
+          })}
+          error={
+            errors?.cedula?.message ||
+            errors?.["cedula-duplicate-error"]?.message
+          }
+        />
+
+        <Controller
+          control={control}
+          name="sexo"
+          render={({ field: { onChange, value, ref } }) => (
+            <SelectInput
+              label="Género"
+              id="student-sexo"
+              value={value}
+              inputRef={ref}
+              onChange={onChange}
+              options={[
+                { value: "M", display: "Masculino" },
+                { value: "F", display: "Femenino" },
+              ]}
+            />
+          )}
+        />
+
+        <Button size="large" variant="contained" color="success" type="submit">
+          Añadir estudiante
+        </Button>
+      </CustomForm>
     </>
   );
 }
