andresgdiazr/Front-End-Servicio

commit 8f669b7acf755a381fede5496c39557576bdf2dd

refactor SeccionDashboard to use less code and better practices

src/routes/Seccion/SeccionDashboard.jsx
@@ -35,43 +35,18 @@ function SeccionDashboard() {
 		const fetchClases = async () => {
 
 			dispatch(setLoading(true))
-			const SeccionesRes = await getSecciones();
+			const seccionesRes = await getSecciones();
 			dispatch(setLoading(false))
 
+			seccionRes.sort( (a,b) => a.año > b.año ? 1 : -1 )
 
-
-			const PrimerAño = SeccionesRes.filter((el) => {
-				if (el.año === 1) {
-					return el;
-				}
-			});
-
-			const SegundoAño = SeccionesRes.filter((el) => {
-				if (el.año === 2) {
-					return el;
-				}
-			});
-
-			const TerceroAño = SeccionesRes.filter((el) => {
-				if (el.año === 3) {
-					return el;
-				}
-			});
-
-			const CuartoAño = SeccionesRes.filter((el) => {
-				if (el.año === 4) {
-					return el;
-				}
-			});
-
-			const QuintoAño = SeccionesRes.filter((el) => {
-				if (el.año === 5) {
-					return el;
-				}
-			});
+			const PrimerAño = seccionesRes.filter((seccion) => seccion.año == 1);
+			const SegundoAño = seccionesRes.filter((seccion) => seccion.año == 2);
+			const TerceroAño = seccionesRes.filter((seccion) => seccion.año == 3);
+			const CuartoAño = seccionesRes.filter((seccion) => seccion.año == 4);
+			const QuintoAño = seccionesRes.filter((seccion) => seccion.año == 5);
 
 			setAños([
-				...años,
 				PrimerAño,
 				SegundoAño,
 				TerceroAño,
@@ -109,17 +84,6 @@ function SeccionDashboard() {
 
 	const AñoItem = ({ secciones }) => {
 		const [expanded, setExpanded] = useState(false);
-
-		secciones.sort(function (a, b) {
-			if (a.codigo < b.codigo) {
-				return -1;
-			}
-			if (a.codigo > b.codigo) {
-				return 1;
-			}
-			return 0;
-		});
-
 		return (
 			<>
 				<ListItem disablePadding onClick={() => setExpanded(!expanded)}>

commit f1295e588b2ae560b86fb8f9a7b62283766f5459

fix bug introduced with commit 8f669b7 and added loader for seccion students

src/routes/Seccion/SeccionDashboard.jsx
@@ -37,7 +37,7 @@ function SeccionDashboard() {
       const seccionesRes = await getSecciones();
       dispatch(setLoading(false));
 
-      seccionRes.sort((a, b) => (a.año > b.año ? 1 : -1));
+      seccionesRes.sort((a, b) => (a.año > b.año ? 1 : -1));
 
       const PrimerAño = seccionesRes.filter((seccion) => seccion.año == 1);
       const SegundoAño = seccionesRes.filter((seccion) => seccion.año == 2);

src/routes/Seccion/SeccionEstudiantes.jsx
@@ -7,6 +7,8 @@ import { Estudiantes_table } from "../../components/tables/EstudiantesTable";
 import TablaBusqueda from "../../components/tables/GenericSearchTable";
 
 import EditIcon from "@mui/icons-material/Edit";
+import { useDispatch } from "react-redux";
+import { setLoading } from "../../store/features/main";
 
 
 function SeccionEstudiantes() {
@@ -23,10 +25,14 @@ function SeccionEstudiantes() {
     setText(lowerCase);
   };
 
+  const dispatch = useDispatch()
+
   useEffect(() => {
 
     const fetchProfesores = async () => {
+      dispatch(setLoading(true));
       let estudiantesRes = await getEstudiantes(id);
+      dispatch(setLoading(false));
 
 
       let estudianteRes = estudiantesRes.map((estudiante) => {