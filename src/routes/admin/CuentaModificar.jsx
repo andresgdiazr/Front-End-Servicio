62fbea3ea2b238bc32f7c194d16c0dd897ff2924
Jose Marquez <josemmarquezlven@gmail.com>
2023-05-02 07:45:50 -0400

    use setSucess action in cuentaModificar to notify user of success op, and also redirect user after editing profesor

--- src/routes/admin/CuentaModificar.jsx
+++ src/routes/admin/CuentaModificar.jsx
@@ -3,25 +3,25 @@ import { useDispatch } from "react-redux";
 import { useNavigate, useLocation, useParams } from "react-router-dom";
 import { updateCuenta } from "../../api/updateCuenta";
 import CuentaForm from "../../components/organisms/CuentaForm";
-import { setLoading } from "../../store/features/main";
-import { Alert, css, Snackbar } from "@mui/material";
+import { setLoading, setSucess } from "../../store/features/main";
 
 function CuentaModificar({ tipo }) {
-	
-  const [open, setOpen] = useState(false);
+
 	const [usedEmails, setUsedEmails] = useState([]);
   const [usedCedulas, setUsedCedulas] = useState([]);
 	
   const { state } = useLocation();
   const {id : profesorId} = useParams();
   const dispatch = useDispatch();
+	const navigate = useNavigate();
 
   const onSubmit = async (data) => {
 		dispatch(setLoading(false));
 		const response = await updateCuenta(tipo, profesorId, data);
 		dispatch(setLoading(false));
     if (response.status == 200) {
-      setOpen(true);
+			dispatch(setSucess('Cuenta modificada satisfactoriamente'))
+			navigate('/dashboard-control/admin/profesores')
     } else {
       if (
         response.data.errors.some(
@@ -48,20 +48,6 @@ function CuentaModificar({ tipo }) {
 
   return (
     <div>
-      <Snackbar
-        css={css`
-          svg {
-            color: white;
-          }
-        `}
-        open={open}
-        autoHideDuration={1000}
-        onClose={() => setOpen(false)}
-      >
-        <Alert variant="filled" severity="success">
-          Cuenta modificada satisfactoriamente
-        </Alert>
-      </Snackbar>
       <h2>Administración de {tipo}</h2>
       <h3>Modificando información de la cuenta</h3>
 
