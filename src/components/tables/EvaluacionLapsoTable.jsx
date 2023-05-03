4d76871ec82b83ddd6fc19351f8e3c3b099698e6
Jose Marquez <josemmarquezlven@gmail.com>
2023-05-03 12:26:42 -0400

    refactor EvaluacionLapsoTable

--- src/components/tables/EvaluacionLapsoTable.jsx
+++ src/components/tables/EvaluacionLapsoTable.jsx
@@ -1,16 +1,15 @@
 import { css } from "@emotion/react";
 import { Book, Visibility } from "@mui/icons-material";
-import { Typography } from "@mui/material";
 import React, { useMemo } from "react";
 import { Link } from "react-router-dom";
 import { useTable } from "react-table";
 import THead from "../molecules/THead";
 
-function EvaluacionLapsoTable({ claseId, materiaData = {} }) {
+function EvaluacionLapsoTable({ materiaData = {} }) {
   const columns = useMemo(
     () => [
       { Header: "Lapso", accessor: "lapso" },
-      { Header: "Acción", accessor: "accion" },
+      { Header: "Acciónes", accessor: "acciones" },
     ],
     []
   );
@@ -32,77 +31,56 @@ function EvaluacionLapsoTable({ claseId, materiaData = {} }) {
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
     tableInstance;
 
-  return (
-    <div
-      css={css`
-        h2 {
-          font-size: 1.6rem;
-          margin-bottom: 1rem;
-        }
-        width: 50%;
-        margin: 1rem auto;
-      `}
-    >
-      <Typography variant="h2">Evaluaciones por Lapso</Typography>
-      <table
+  const CellContent = ({ cell }) => {
+    if (cell.column.id === 'acciones') {
+      return (
+        <div
         css={css`
-          td,
-          th {
-            text-align: center;
+          display: flex;
+          justify-content: center;
+          svg {
+            margin: 0 1rem;
           }
         `}
-        {...getTableProps()}
       >
-        <THead headerGroups={headerGroups} />
-        {/* Apply the table body props */}
-        <tbody {...getTableBodyProps()}>
-          {
-            // Loop over the table rows
-            rows.map((row) => {
-              // Prepare the row for display
-              prepareRow(row);
-              return (
-                // Apply the row props
-                <tr {...row.getRowProps()}>
-                  {
-                    // Loop over the rows cells
-                    row.cells.map((cell) => {
-                      // Apply the cell props
-                      return (
-                        <td {...cell.getCellProps()}>
-                          {cell.value ? (
-                            cell.render("Cell")
-                          ) : (
-                            <div
-                              css={css`
-                                display: flex;
-                                justify-content: center;
-                                svg {
-                                  margin: 0 1rem;
-                                }
-                              `}
-                            >
-                              <Link
-                                state={{ ...materiaData }}
-                                to={`lapsos/${cell.row.original.lapsoNumber}/evaluaciones`}
-                              >
-                                <Visibility />
-                              </Link>
+        <Link
+          state={{ ...materiaData }}
+          to={`lapsos/${cell.row.original.lapsoNumber}/evaluaciones`}
+        >
+          <Visibility />
+        </Link>
 
-                              <Book />
-                            </div>
-                          )}
-                        </td>
-                      );
-                    })
-                  }
-                </tr>
-              );
-            })
-          }
-        </tbody>
-      </table>
-    </div>
+        <Book />
+      </div>
+      )
+    }
+    return cell.render("Cell");
+  };
+
+  return (
+    <table
+      css={css`
+        td,
+        th {
+          text-align: center;
+        }
+      `}
+      {...getTableProps()}
+    >
+      <THead headerGroups={headerGroups} />
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}> <CellContent  cell={cell} /> </td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 }
 
--- src/routes/Profesor/Clase.jsx
+++ src/routes/Profesor/Clase.jsx
@@ -1,27 +1,39 @@
-
 import React from "react";
 import { useLocation, useParams } from "react-router-dom";
 import ClaseInfo from "../../components/organisms/ClaseInfo";
-
 import EvaluacionLapsoTable from "../../components/tables/EvaluacionLapsoTable";
+import { Typography, css } from "@mui/material";
 
 function Clase() {
-	const {
-		state: { materia, clase },
-	} = useLocation();
-	const { id: claseId } = useParams();
-	const materiaData = { materia, clase };
-	return (
-		<div>
-			<ClaseInfo
-				materia={materia.nombre}
-				año={materia.año}
-				seccion={clase.seccion.codigo}
-			/>
+  const {
+    state: { materia, clase },
+  } = useLocation();
+  const { id: claseId } = useParams();
+  const materiaData = { materia, clase };
+  return (
+    <div>
+      <ClaseInfo
+        materia={materia.nombre}
+        año={materia.año}
+        seccion={clase.seccion.codigo}
+      />
 
-			<EvaluacionLapsoTable claseId={claseId} materiaData={materiaData} />
-		</div>
-	);
+      <div
+        css={css`
+          h2 {
+            font-size: 1.6rem;
+            margin-bottom: 1rem;
+          }
+          width: 50%;
+          margin: 1rem auto;
+        `}
+      >
+        <Typography variant="h2">Evaluaciones por Lapso</Typography>
+
+        <EvaluacionLapsoTable claseId={claseId} materiaData={materiaData} />
+      </div>
+    </div>
+  );
 }
 
 export default Clase;
