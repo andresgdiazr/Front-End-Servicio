82b270c094a4e02a1f3dc23489989c0cb2cfdcf0
ReynerCr <25756546+ReynerCr@users.noreply.github.com>
2023-05-17 00:31:37 -0400

    Changed behaviour so now it works with profesor accounts. Needs refactor for conditions.

--- src/components/ProfesorTitleAdmin.jsx
+++ src/components/ProfesorTitleAdmin.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import GenericTitles from "components/GenericTitles";
 import { useProfesorData } from "store/features/navigationData";
+import { useSelector } from "react-redux";
 
 function ProfesorTitleAdmin({ title = "", prevSubtitles = [], newSubtitle }) {
-	const profesor = useProfesorData();
-	if (profesor) { // TODO no hay una mejor solucion?
-		prevSubtitles.unshift(`Profesor: ${profesor.nombre} ${profesor.apellido}`);
+	// Change function if administrador or profesor
+	const userType = sessionStorage.getItem("user-type");
+	const profesor =
+		userType === "Administrador"
+			? useProfesorData()
+			: useSelector((state) => state.main.name);
+	console.log(profesor);
+
+	if (profesor) {
+		// TODO no hay una mejor solucion?
+		prevSubtitles.unshift(
+			`Profesor: ` + (userType === "Administrador"
+				? `${profesor.nombre} ${profesor.apellido}`
+				: profesor)
+		);
 	}
 
 	return (
 		<GenericTitles
-			title={!title ? "Administración de profesores" : title}
+			title={!title ? (userType === "Administrador" ? "Administración de profesores" : "Información de materia") : title}
 			prevSubtitles={prevSubtitles}
 			newSubtitle={newSubtitle}
 		/>
