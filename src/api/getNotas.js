0e32c271f4da26239e2915f9ce629117cc2e9dec
Jose Marquez <josemmarquezlven@gmail.com>
2023-05-17 11:17:37 -0400

    updated routes to get and upload notas to fix bug that return 403 to admin when viewing notas

--- src/api/getNotas.js
+++ src/api/getNotas.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getNotas = ({ clase, evaluacion }) => {
   return axios
-    .get(`/profesor/clases/${clase}/evaluaciones/${evaluacion}/calificaciones`)
+    .get(`/clases/${clase}/evaluaciones/${evaluacion}/calificaciones`)
     .then( response => response.data )
     .catch((err) => console.log(err));
 };
