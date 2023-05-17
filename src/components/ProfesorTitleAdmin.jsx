8e07a2a5868b5260ac410800632029e92353e4a3
ReynerCr <25756546+ReynerCr@users.noreply.github.com>
2023-05-17 10:25:49 -0400

    fix: removed unshifted and console log

--- src/components/ProfesorTitleAdmin.jsx
+++ src/components/ProfesorTitleAdmin.jsx
@@ -4,42 +4,36 @@ import { useProfesorData } from "store/features/navigationData";
 import { useSelector } from "react-redux";
 
 function ProfesorTitleAdmin({ title = "", prevSubtitles = [], newSubtitle }) {
-  // Change function if administrador or profesor
-  const userType = sessionStorage.getItem("user-type");
-  const profesor =
-    userType === "Administrador"
-      ? useProfesorData()
-      : useSelector((state) => state.main.name);
-  console.log(profesor);
+	// Change function if administrador or profesor
+	const userType = sessionStorage.getItem("user-type");
+	const profesor =
+		userType === "Administrador"
+			? useProfesorData()
+			: useSelector((state) => state.main.name);
 
-  if (profesor) {
-    // TODO no hay una mejor solucion?
-    prevSubtitles.unshift();
-  }
-
-  return (
-    <GenericTitles
-      title={
-        !title
-          ? userType === "Administrador"
-            ? "Administración de profesores"
-            : "Información de materia"
-          : title
-      }
-      prevSubtitles={[
-        ...(profesor
-          ? [
-              `Profesor: ` +
-                (userType === "Administrador"
-                  ? `${profesor.nombre} ${profesor.apellido}`
-                  : profesor),
-            ]
-          : []),
-        ...prevSubtitles,
-      ]}
-      newSubtitle={newSubtitle}
-    />
-  );
+	return (
+		<GenericTitles
+			title={
+				!title
+					? userType === "Administrador"
+						? "Administración de profesores"
+						: "Información de materia"
+					: title
+			}
+			prevSubtitles={[
+				...(profesor
+					? [
+							`Profesor: ` +
+								(userType === "Administrador"
+									? `${profesor.nombre} ${profesor.apellido}`
+									: profesor),
+					  ]
+					: []),
+				...prevSubtitles,
+			]}
+			newSubtitle={newSubtitle}
+		/>
+	);
 }
 
 export default ProfesorTitleAdmin;
